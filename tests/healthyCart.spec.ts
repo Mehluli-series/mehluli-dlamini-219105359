7dd62d2ca4f8eee33e894977a1d8ea83d7a491e3
Mehluli Dlamini <mehlulidlamini@Mehlulis-Visual.local>
2025-02-16 12:55:54 +0200

    Initial Commit

---
+++ tests/healthyCart.spec.ts
@@ -0,0 +1,56 @@
+import { test, expect } from '@playwright/test';
+
+test('add to cart test', async ({ page }) => {
+await page.goto('https://www.woolworths.co.za/');
+await page.getByText('Delivery Address').click();
+await page.getByRole('textbox', { name: 'Enter your street address eg.' }).click();
+await page.getByRole('textbox', { name: 'Enter your street address eg.' }).fill('97 ');
+await page.getByRole('textbox', { name: 'Enter your street address eg.' }).press('CapsLock');
+await page.getByRole('textbox', { name: 'Enter your street address eg.' }).fill('97 Upper ');
+await page.getByRole('textbox', { name: 'Enter your street address eg.' }).press('CapsLock');
+await page.getByRole('textbox', { name: 'Enter your street address eg.' }).fill('97 Upper Par');
+await page.getByRole('option', { name: '97 Upper Park Road, Walmer' }).click();
+await page.getByText('Confirm address').click();
+await page.locator('div').filter({ hasText: /^Click & Collect$/ }).first().click();
+await page.getByText('Confirm Collection').click();
+await page.getByRole('search', { name: 'Search bar input area' }).click();
+await page.getByRole('search', { name: 'Search bar input area' }).press('CapsLock');
+await page.getByRole('search', { name: 'Search bar input area' }).fill('Mango');
+await page.getByRole('search', { name: 'Search bar input area' }).press('Enter');
+await page.getByRole('link', { name: 'Woolies Brands Mango Snackpot' }).click();
+await page.locator('div').filter({ hasText: /^1$/ }).nth(2).click();
+await page.locator('[id="\\36 009189559150"]').getByText('2', { exact: true }).click();
+await page.getByText('Add to Cart').click();
+await page.getByRole('search', { name: 'Search bar input area' }).click();
+await page.getByRole('search', { name: 'Search bar input area' }).press('CapsLock');
+await page.getByRole('search', { name: 'Search bar input area' }).fill('Grapes');
+await page.getByRole('search', { name: 'Search bar input area' }).press('Enter');
+await page.getByRole('link', { name: 'Seedless Black Grapes 500 g' }).first().click();
+await page.getByText('VIEW', { exact: true }).click();
+await page.locator('div').filter({ hasText: /^Gardens Centre, Cnr Buitenkant St & Mill St, Gardens, 8001Store Details$/ }).locator('span').click();
+await page.locator('.fis-rotating-indicator-main > div:nth-child(3)').click();
+await page.getByText('Add to Cart').click();
+await page.getByRole('link', { name: 'Checkout mini cart' }).click();
+await page.getByRole('button', { name: 'dismiss cookie message' }).click();
+await page.getByRole('banner').getByRole('link').filter({ hasText: /^$/ }).click();
+await page.getByRole('search', { name: 'Search bar input area' }).click();
+await page.getByRole('search', { name: 'Search bar input area' }).press('CapsLock');
+await page.getByRole('search', { name: 'Search bar input area' }).fill('Onions');
+await page.getByRole('search', { name: 'Search bar input area' }).press('Enter');
+await page.getByRole('link', { name: 'Woolies Brands Red Onions 1 kg' }).click();
+await page.getByText('Add to Cart').click();
+await page.getByRole('search', { name: 'Search bar input area' }).click();
+await page.getByRole('search', { name: 'Search bar input area' }).press('CapsLock');
+await page.getByRole('search', { name: 'Search bar input area' }).fill('Potatoes');
+await page.getByRole('search', { name: 'Search bar input area' }).press('Enter');
+await page.getByRole('link', { name: 'Woolies Brands Baby Potatoes with Garlic & Herb Butter 350 g' }).click();
+await page.locator('div').filter({ hasText: /^1$/ }).nth(2).click();
+await page.locator('[id="\\32 0046002"]').getByText('2').click();
+await page.getByText('Add to Cart').click();
+await page.getByRole('search', { name: 'Search bar input area' }).click();
+await page.getByRole('search', { name: 'Search bar input area' }).fill('cook book');
+await page.locator('#prod_details_undefined').getByRole('link', { name: 'My First Vegetarian Cook Book' }).click();
+await page.getByText('Add to Cart').click();
+});
+
+ 
---
+++ tests/vegetarian.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('find recipes', async ({ page }) => {
+await page.goto('https://www.whatsfordinner.co.za/');
+await page.getByRole('button', { name: 'Accept' }).click();
+await page.getByRole('menubar').getByText('Recipes', { exact: true }).click();
+await page.getByRole('link', { name: 'Vegetarian Recipes' }).click();
+await page.getByRole('link', { name: 'Plant-based Recipes' }).click();
+await page.getByRole('link', { name: 'Buffalo Cauliflower Tacos' }).getByLabel('inactive Favorite toggle').click();
+await page.getByRole('link', { name: 'Buffalo Cauliflower Tacos' }).click();
+await page.getByRole('button', { name: 'print recipe' });
+});
