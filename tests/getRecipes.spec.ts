83802448213651abce507fdb4c21034b3e885487
mehluli-series <mehluliseries@outlook.com>
2025-02-16 17:53:01 +0200

    Add download recipe (pdf) format

--- tests/getRecipes.spec.ts
+++ tests/getRecipes.spec.ts
@@ -9,5 +9,12 @@ await page.getByRole('link', { name: 'Plant-based Recipes' }).click();
 await page.getByRole('link', { name: 'Buffalo Cauliflower Tacos' }).getByLabel('inactive Favorite toggle').click();
 await page.getByRole('link', { name: 'Buffalo Cauliflower Tacos' }).click();
 await page.getByRole('button', { name: 'print recipe' });
-await page.pdf({ path: './Recipes/YourRecipe.pdf', format: 'A4' });
+});
+
+var recipeUrl = 'https://www.whatsfordinner.co.za/recipe/vegetables/234714-buffalo-cauliflower-tacos/';
+
+test ('download your recipe', async({page})=> {
+    //This test to download pdf document for the above recipe - Buffalo Cauliflower Tacos
+    await page.goto(recipeUrl)
+    await page.pdf({ path: './Recipes/CauliflowerRecipe.pdf', format: 'A4' });
 });
